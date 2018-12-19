2d20d38b96754856880dde4d1d60ccbf299a65cc
pirest89 <thang89@gmail.com>
2018-12-19 22:45:53 +0700

    inital project

---
+++ deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func getNewDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Diamonds", "Hearts", "Clubs", "Spades"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// var card = "Ace of Spades"
+	cards := getNewDeck()
+	cards.print()
+}
